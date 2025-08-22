9ee973df340121667ed370a7519f792cfe356993
GraphiWebAvenue <graphiweb.avenue@gmail.com>
2025-08-22 21:05:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Buttons/Button";
+import { Input } from "./components/Forms/Input";
+
+function App() {
+  return (
+    <>
+      <Button></Button>
+      <Input></Input>
+    </>
+  );
+}
+
+export default App;
