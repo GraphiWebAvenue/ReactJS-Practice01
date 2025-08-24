0bab82cf3bc26ccf8ac521d0f5f245a371282d69
GraphiWebAvenue <graphiweb.avenue@gmail.com>
2025-08-24 13:59:07 +0200

    add inlineCSS

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,9 +1,13 @@
 import "./Button.css";
 
 export const Button = () => {
+  const myStyle = {
+    width: "100%",
+    height: "200px",
+  };
   return (
     <>
-      <button className="headerbtn">منو بمال!</button>
+      <button style={myStyle}>منو بمال!</button>
     </>
   );
 };
