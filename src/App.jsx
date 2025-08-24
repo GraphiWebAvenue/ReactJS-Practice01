31dbc2dcfac8306badb7c19c7c9e9efb2c59945c
GraphiWebAvenue <graphiweb.avenue@gmail.com>
2025-08-24 22:36:20 +0200

    before fix of useState and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ import { Form } from "./components/Forms/Form";
 import { Input } from "./components/Forms/Input";
 
 function App() {
+  let count = 0;
+  const handleClick = () => {
+    count++;
+    console.log(count);
+  };
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <>
+      <h2>{count}</h2>
+      <button className="Erfan" onClick={handleClick}>
+        Click
+      </button>
+      <input placeholder="In ye Test Hast" onChange={handleChange}></input>
       <Form title_form="Login Form" btn_text_form="Login">
         <Input type={"email"} label_text={"Enter Your Email:"}></Input>
         <Input type={"password"} label_text={"Enter Your Password:"}></Input>
